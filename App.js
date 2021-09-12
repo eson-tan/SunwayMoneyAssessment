403fe0615407c4ba296c897e5ee150e04a755f1b
Eson Tan <tanesonjpg@gmail.com>
2021-09-12 15:56:33 +0800

    added stack navigator

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Home from './src/scenes/home';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import NewCustomer from './src/scenes/addNewCustomer';
+import {NavigationContainer} from '@react-navigation/native';
 
-const App = () => {
-  return <Home />;
+const Stack = createNativeStackNavigator();
+
+function AppStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="NewCustomer" component={NewCustomer} />
+    </Stack.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppStack />
+    </NavigationContainer>
+  );
 };
 
-export default App;
+// export default App;
